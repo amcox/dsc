efb584999be2a7bbe4e0ee40bbe8c51b2877d384
amcox <andrewmcox@gmail.com>
2015-06-28 19:29:26 -0500

    updated export for TIF, added long formatting to load data functions

--- R/export for TIF.r
+++ R/export for TIF.r
@@ -10,14 +10,11 @@ update_functions <- function() {
 }
 update_functions()
 
-df <- load_dsc_data()
-dm <- df %>%
-  gather(measure, npg, -c(id, first.name, last.name, teacher, school), na.rm = TRUE) %>%
-  separate(measure, c('round', 'subject'))
-dm$npg[dm$npg == 0] <- 1
-dm$nce <- make_nce_from_percentile(dm$npg)
+d <- load_dsc_data_long(y=2015)
 
-names(dm)[names(dm) == 'id'] <- 'student_number'
-dm <- dm[, c('student_number', 'round', 'subject', 'nce')]
+d$npr[d$npr == 0] <- 1
+d$nce <- make_nce_from_percentile(d$npr)
 
-save_df_as_csv(dm, 'dsc data for tif')
+d <- d %>% select(id, round, subject, nce)
+
+save_df_as_csv(d, 'dsc data for tif')
